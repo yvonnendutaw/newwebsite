ad8b44dac8f4844cdcfd60c3fb6a5b524ec6d72c
Yvonne Nduta <yvonnendutaw@users.noreply.github.com>
2017-02-13 17:54:23 +0300

    changed nav

--- js/testimonials.js
+++ js/testimonials.js
@@ -1,15 +1,3 @@
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-}
-/* Set the width of the side navigation to 0 */
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-}
-$(document).ready(function () {
-    $('.slider').slider({
-        full_width: true
-    });
-});
 jQuery(document).ready(function ($) {
     //create the slider
     $('.cd-testimonials-wrapper').flexslider({
