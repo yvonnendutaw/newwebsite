4d6239a19de0d3330127d036081d97bb171c2e66
Yvonne Nduta <yvonnendutaw@users.noreply.github.com>
2017-02-15 16:24:04 +0300

    firstfinals

--- js/app.js
+++ js/app.js
@@ -26,3 +26,13 @@ jQuery(document).ready(function ($) {
         }
     });
 });
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            $('.navbar').fadeIn(500);
+        }
+        else {
+            $('.navbar').fadeOut(500);
+        }
+    });
+});
