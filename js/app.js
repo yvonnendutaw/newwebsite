66d655aab69b3cb79aa5a9e26f5bb4731968647c
Yvonne Nduta <yvonnendutaw@users.noreply.github.com>
2017-02-21 09:48:56 +0300

    added image

--- js/app.js
+++ js/app.js
@@ -4,17 +4,35 @@ $(document).ready(function () {
     });
 });
 jQuery(document).ready(function ($) {
-            //create the slider
-            $('.cd-testimonials-wrapper').flexslider({
-                selector: ".cd-testimonials > li"
-                , animation: "slide"
-                , controlNav: true
-                , slideshow: false
-                , smoothHeight: true
-                , start: function () {
-                    $('.cd-testimonials').children('li').css({
-                        'opacity': 1
-                        , 'position': 'relative'
-                    });
-                }
+    //create the slider
+    $('.cd-testimonials-wrapper').flexslider({
+        selector: ".cd-testimonials > li"
+        , animation: "slide"
+        , controlNav: true
+        , slideshow: false
+        , smoothHeight: true
+        , start: function () {
+            $('.cd-testimonials').children('li').css({
+                'opacity': 1
+                , 'position': 'relative'
             });
+        }
+    });
+});
+$(window).scroll(function () {
+    if ($(window).scrollTop() >= 500) {
+        $('.navbar').css('background', 'white');
+    }
+    else {
+        $('.navbar').css('background', 'transparent');
+    }
+});
+//$(window).scroll(function () {
+//            if ($(document).scrollTop() == 0) {
+//                $('.title-area img').attr('src', 'img/newnetlogo.png');
+//            }
+//            else if {
+//                ($(document).scrollTop() >= 500) {
+//                    $('.title-area img').attr('src', 'img/Netwookie%20Logo%20Horizontal.png');
+//                }
+//            });
